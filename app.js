6adcc1dd727dd5819571b81ad05930554b512eeb
Michael Rubino <parkcathedralgrace@gmail.com>
2019-07-01 23:49:01 -0400

    Express

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const m
+
+const app = express();
 
 app.use('/',express.static('public'));
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    next();
+});
 
 app.get('/', (req,res)=>{
 
